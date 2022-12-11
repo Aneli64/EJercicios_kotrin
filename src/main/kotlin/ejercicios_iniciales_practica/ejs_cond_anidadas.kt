17048744be098b6934d3d2966033b9a073bdefed
aneli <eliandeniz27@gmail.com>
2022-12-11 19:59:27 +0100

    cambios

---
+++ src/main/kotlin/ejercicios_iniciales_practica/ejs_cond_anidadas.kt
@@ -0,0 +1,47 @@
+package ejercicios_iniciales_practica
+
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
+fun main(){
+    /*
+    Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.
+    println("v1: ")
+    var valor1 = readln().toInt()
+    println("v2: ")
+    var valor2 = readln().toInt()
+    println("v3: ")
+    var valor3 = readln().toInt()
+
+    var list = listOf(valor1, valor2, valor3)
+    println("Valor mayor -> ${list.max()}")
+     */
+    /*
+    Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el número
+    es positivo, nulo o negativo.
+
+    println("valor: ")
+    val valor = readln()
+    //revisar el nulo
+        if (valor.toInt() >= 0) print("Numero positivo")
+        else if (valor.toInt() < 0) print("Numero negativo")
+        else if (valor == "") println("Numero nulo")
+    */
+    /*
+    Confeccionar un programa que permita cargar un número entero positivo de hasta tres cifras y
+    muestre un mensaje indicando si tiene 1, 2, o 3 cifras. Mostrar un mensaje de
+    error si el número de cifras es mayor.
+    */
+
+
+
+    /*
+    Un postulante a un empleo, realiza un test de capacitación, se obtuvo
+    la siguiente información:
+    cantidad total de preguntas que se le realizaron y la cantidad de preguntas
+    que contestó correctamente.
+    Se pide confeccionar un programa que ingrese los dos datos por
+    teclado e informe el nivel del mismo según el porcentaje de respuestas
+    correctas que ha obtenido, y sabiendo que
+    */
+}
---
+++ src/main/kotlin/ejercicios_iniciales_practica/ejs_condicionales.kt
@@ -0,0 +1,34 @@
+package ejercicios_iniciales_practica
+
+fun main() {
+    /*
+Se ingresan tres notas de un alumno, si el promedio es mayor o igual a siete
+mostrar un mensaje "Promocionado".
+
+    println("Introduzca su primera nota: ")
+    var nota1 = readln().toFloat()
+    println("Introduzca su segunda nota: ")
+    var nota2 = readln().toFloat()
+    println("Introduzca su tercera nota: ")
+    var nota3 = readln().toFloat()
+
+    var prom = (nota1+nota2+nota3)/3
+    if (prom >= 7) println("promocionado") else println("no promocionado")
+     */
+
+    /*
+Se ingresa por teclado un número entero comprendido entre 1 y 99, mostrar un mensaje indicando si el
+número tiene uno o dos dígitos.(Tener en cuenta que condición debe cumplirse para
+tener dos dígitos, un número entero)
+
+    //falta la exception en caso de float con un decimal
+    println("Introduzca un numero entero: ")
+    var numero = readln().toInt()
+
+    if (numero.toString().length > 2) println("Numero no valido")
+    else {
+        if (numero.toString().length == 2) print("Su numero cuenta con dos cifras")
+        else println("Su numero cuenta con una cifra")
+    }
+     */
+}
