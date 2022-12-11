5bdc23d4889b74d2af384201f30ec50f479375dd
aneli <eliandeniz27@gmail.com>
2022-12-11 18:55:25 +0100

    cambios

---
+++ src/main/kotlin/ejercicios_iniciales_practica/ej1.kt
@@ -0,0 +1,66 @@
+package ejercicios_iniciales_practica
+
+fun main() {
+    /*Problema 1
+    Realizar la carga de dos números enteros por teclado e imprimir su suma y su producto.
+
+    print("Introduzca un valor: \n")
+    var v1 = readln().toInt()
+    print("Introduzca un segundo valor: \n")
+    var v2 = readln().toInt()
+
+    var vRes = v1 + v2
+    println("La suma de sus valores es -> $vRes")
+    */
+
+    /*Problema 2
+    Se debe desarrollar un programa que pida el ingreso del precio de un artículo y la cantidad que
+    lleva el cliente. Mostrar lo que debe abonar el comprador.
+
+    println("Introduzca el precio de su articulo: ")
+    var precioArt = readln().toInt()
+    println("Introduzca la cantidad: ")
+    var cant = readln().toInt()
+
+    var res = precioArt * cant
+    println("Su precio a pagar seria -> $res")
+     */
+    /*Problema 3
+    Escribir un programa en el cual se ingresen cuatro números enteros, calcular e informar la
+    suma de los dos primeros y el producto del tercero y el cuarto.
+
+    println("Introduzca un valor: ")
+    var v1 = readln().toInt()
+    println("Introduzca un valor: ")
+    var v2 = readln().toInt()
+    println("Introduzca un valor: ")
+    var v3 = readln().toInt()
+    println("Introduzca un valor: ")
+    var v4 = readln().toInt()
+
+    var suma1y2 = v1 + v2
+    var producto3y4 = v3 * v4
+
+    println("La suma de los dos primeros valores es -> $suma1y2 \n y el producto de los " +
+            "valores tres y cuatro es -> $producto3y4")
+     */
+    /*Problema 4
+    Realizar un programa que lea por teclado cuatro valores numéricos
+    enteros e informar su suma y promedio.
+
+    println("Introduzca un valor: ")
+    var v1 = readln().toInt()
+    println("Introduzca un valor: ")
+    var v2 = readln().toInt()
+    println("Introduzca un valor: ")
+    var v3 = readln().toInt()
+    println("Introduzca un valor: ")
+    var v4 = readln().toInt()
+
+    var suma = v1 + v2 + v3 + v4
+    var prom = suma / 4
+
+    println("Suma de sus valores -> $suma \n Promedio -> $prom")
+     */
+}
+
