0a355955ab340ca1e5d067d88a6b89269f0007bd
aneli <eliandeniz27@gmail.com>
2023-01-20 20:51:57 +0100

    Proyecto black jack

--- src/main/kotlin/ejercicios_practica_internet/ejs_POO/ejBlackJack.kt
+++ src/main/kotlin/ejercicios_practica_internet/ejs_POO/ejBlackJack.kt
@@ -4,9 +4,9 @@ class Partida(var n_jugadores:Int) {
         println("Introduzca numero de jugadores")
         val n_jugs = readln()
 
-        while (!cond_vic()) {
+        /*while (!cond_vic()) {
 
-        }
+        }*/
     }
 
     fun turno(): Boolean {
@@ -16,39 +16,40 @@ class Partida(var n_jugadores:Int) {
     }
 
     //fun cond_vic():
-fun main() {
-    val barajaCoraz = mutableListOf<String>()
-    for (i in 1..13) {
-        if (i % 11 == 0) barajaCoraz.add("Jota" + "Corazones")
-        else if (i % 12 == 0) barajaCoraz.add("Reina" + "Corazones")
-        else if (i % 13 == 0) barajaCoraz.add("Rey" + "Corazones")
-        else barajaCoraz.add("$i" + "Corazones")
-    }
+    fun main() {
+        val barajaCoraz = mutableListOf<String>()
+        for (i in 1..13) {
+            if (i % 11 == 0) barajaCoraz.add("Jota" + "Corazones")
+            else if (i % 12 == 0) barajaCoraz.add("Reina" + "Corazones")
+            else if (i % 13 == 0) barajaCoraz.add("Rey" + "Corazones")
+            else barajaCoraz.add("$i" + "Corazones")
+        }
 
-    val barajaDiam = mutableListOf<String>()
-    for (i in 1..13) {
-        if (i % 11 == 0) barajaDiam.add("Jota" + "Diamantes")
-        else if (i % 12 == 0) barajaDiam.add("Reina" + "Diamantes")
-        else if (i % 13 == 0) barajaDiam.add("Rey" + "Diamantes")
-        else barajaDiam.add("$i" + "Diamantes")
-    }
+        val barajaDiam = mutableListOf<String>()
+        for (i in 1..13) {
+            if (i % 11 == 0) barajaDiam.add("Jota" + "Diamantes")
+            else if (i % 12 == 0) barajaDiam.add("Reina" + "Diamantes")
+            else if (i % 13 == 0) barajaDiam.add("Rey" + "Diamantes")
+            else barajaDiam.add("$i" + "Diamantes")
+        }
 
-    val barajaTreb = mutableListOf<String>()
-    for (i in 1..13) {
-        if (i % 11 == 0) barajaTreb.add("Jota" + "Treboles")
-        else if (i % 12 == 0) barajaTreb.add("Reina" + "Treboles")
-        else if (i % 13 == 0) barajaTreb.add("Rey" + "Treboles")
-        else barajaTreb.add("$i" + "Treboles")
-    }
+        val barajaTreb = mutableListOf<String>()
+        for (i in 1..13) {
+            if (i % 11 == 0) barajaTreb.add("Jota" + "Treboles")
+            else if (i % 12 == 0) barajaTreb.add("Reina" + "Treboles")
+            else if (i % 13 == 0) barajaTreb.add("Rey" + "Treboles")
+            else barajaTreb.add("$i" + "Treboles")
+        }
 
-    val barajaPicas = mutableListOf<String>()
-    for (i in 1..13) {
-        if (i % 11 == 0) barajaPicas.add("Jota" + "Picas")
-        else if (i % 12 == 0) barajaPicas.add("Reina" + "Picas")
-        else if (i % 13 == 0) barajaPicas.add("Rey" + "Picas")
-        else barajaPicas.add("$i" + "Picas")
-    }
+        val barajaPicas = mutableListOf<String>()
+        for (i in 1..13) {
+            if (i % 11 == 0) barajaPicas.add("Jota" + "Picas")
+            else if (i % 12 == 0) barajaPicas.add("Reina" + "Picas")
+            else if (i % 13 == 0) barajaPicas.add("Rey" + "Picas")
+            else barajaPicas.add("$i" + "Picas")
+        }
 
-    val baraja = barajaCoraz + barajaDiam + barajaTreb + barajaPicas
-    println(baraja)
+        val baraja = barajaCoraz + barajaDiam + barajaTreb + barajaPicas
+        println(baraja)
+    }
 }
