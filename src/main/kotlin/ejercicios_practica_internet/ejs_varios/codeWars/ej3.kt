6dd2fb7e099bbda53acf924687d96f89500173fb
Aneli <eliandeniz27@gmail.com>
2023-03-28 18:25:41 +0200

    ejercicio codewars casi terminado

--- src/main/kotlin/ejercicios_practica_internet/ejs_varios/codeWars/ej3.kt
+++ src/main/kotlin/ejercicios_practica_internet/ejs_varios/codeWars/ej3.kt
@@ -1,16 +1,24 @@
 package ejercicios_practica_internet.ejs_varios.codeWars
 
-    // runTest("aaaabb", Pair('a',4))
-    fun longestRepetition(s: String):Pair<Char?, Int>  {
-        var mapa = mutableMapOf<Char, Int>()
-        //
-        for (item in s) {
-            if (item !in mapa.keys.toString()) mapa[item] = 1 else mapa[item] = mapa[item]!! + 1
-        }
-        return Pair('h', 2)
-        //return mapa.minBy { it.value } revisar, va por aqui
-    }
+fun numberOfPairs(gloves: List<String>) {
+    var noRep = mutableListOf<String>()
+    gloves.forEach { if (it !in noRep) noRep.add(it) }
+    var cont = 0
+    var listaCont = mutableListOf<Int>()
 
-fun main(){
-    print(longestRepetition("aaaabb"))
+    for (item in noRep) {
+        for (item2 in gloves)
+            if (item == item2) cont++
+        listaCont.add(cont)
+        cont = 0
+    }
+    println(listaCont)
+    //return 0
+}
+
+// assertEquals(1, numberOfPairs( arrayOf("red","red").toList()) )
+// assertEquals(0, numberOfPairs( arrayOf("red","green","blue").toList()) )
+// assertEquals(4, numberOfPairs( arrayOf("red","green","blue","blue","red","green","red","red","red").toList()) )
+fun main() {
+    numberOfPairs(arrayOf("red", "green", "blue", "blue", "red", "green", "red", "red", "red").toList())
 }
